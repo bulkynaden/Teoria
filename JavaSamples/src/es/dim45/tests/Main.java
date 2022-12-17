3d98473cdc5d30e64262ee81d6480dcb031b8198
Javier Toás Acín <javiko.tomas@gmail.com>
2022-12-17 12:18:23 +0100

    test

--- JavaSamples/src/es/dim45/tests/Main.java
+++ JavaSamples/src/es/dim45/tests/Main.java
@@ -2,6 +2,8 @@ package es.dim45.tests;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class Main {
@@ -9,5 +11,22 @@ public class Main {
         System.out.println("MIN: " + Thread.MIN_PRIORITY);
         System.out.println("MAX: " + Thread.MAX_PRIORITY);
         System.out.println("NORMAL: " + Thread.NORM_PRIORITY);
+
+        Deque<String> states = new ArrayDeque<String>();
+
+        states.add("Germany");
+        states.add("France");
+        states.push("UK");
+        states.offerLast("Norway");
+
+        String sPop = states.pop();
+        String sPeek = states.peek();
+        String sPeekLast = states.peekLast();
+        states.offer(sPop);
+        String sPollLast = states.pollLast();
+
+        while (states.peek() != null) {
+            System.out.print(states.pop());
+        }
     }
 }
